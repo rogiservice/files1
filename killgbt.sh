34c45c1a912bac308815fe3b1f83e38eb4be2acc
rogiservice <evankingsford@rogiservice.com>
2013-05-18 12:53:59 -0700

    killgbt-mod

--- killgbt.sh
+++ killgbt.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-PS='gbt'
+PS='minerd'
 declare -i FIELD=1
 PID=$( ps -e | grep $PS | cut -d ' ' -f$FIELD )
 
 if [ -z $PID ] ; then  
 	echo "PID could not be mapped to variable. exiting..."
+	exit
+fi
+
+if [ $PID != [:number:]* ] ; then
+	echo "PID is strange string. exiting...	
 	exit
 fi
 
